6981dd840068e2f777befde105a74f07c2c8c9d4
Jonas Wouters <hello@jonaswouters.be>
2011-03-13 22:49:00 +0100

    Changed yml configuration to xml configuration

--- DependencyInjection/JnsXhprofExtension.php
+++ DependencyInjection/JnsXhprofExtension.php
@@ -3,21 +3,58 @@
 namespace Jns\Bundle\XhprofBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
 class JnsXhprofExtension extends Extension
 {
+    /**
+     * Xml config files to load
+     * @var array
+     */
+    protected $resources = array(
+        'config' => 'config.xml',
+        'services' => 'services.xml',
+    );
+    
+    /**
+     * Loads the services based on your application configuration.
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yml');
-        $loader->load('services.yml');
+        $config = array_shift($configs);
+        foreach ($configs as $tmp) {
+            $config = array_replace_recursive($config, $tmp);
+        }
+
+        $loader = $this->getFileLoader($container);
+        $loader->load($this->resources['config']);
+        $loader->load($this->resources['services']);
+
+        foreach ($config as $key => $value) {
+            $container->setParameter($this->getAlias().'.'.$key, $value);
+        }
+        
     }
 
     public function getAlias()
     {
         return 'jns_xhprof';
     }
+
+    /**
+     * Get File Loader
+     *
+     * @param ContainerBuilder $container
+     */
+    public function getFileLoader($container)
+    {
+        return new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+    
 }
