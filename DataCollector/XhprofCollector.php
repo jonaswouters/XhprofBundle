4be5eae78fc18f433ddf46313595974d5c40224c
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-12-16 14:57:48 +0100

    add the other keys also

--- DataCollector/XhprofCollector.php
+++ DataCollector/XhprofCollector.php
@@ -35,6 +35,8 @@ class XhprofCollector extends DataCollector
         $this->logger    = $logger;
         $this->doctrine  = $doctrine;
         $this->data['xhprof'] = null;
+        $this->data['source'] = null;
+        $this->data['xhprof_url'] = null;
     }
 
     /**
