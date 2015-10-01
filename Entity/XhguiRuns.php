8836e2ed08752c34a75c4f1ee00935cccccd0a32
Adam Heinz <aheinz@stanleygibbons.com>
2015-10-01 09:38:53 -0400

    Remove trait use to placate PHP 5.3

--- Entity/XhguiRuns.php
+++ Entity/XhguiRuns.php
@@ -4,11 +4,24 @@ namespace Jns\Bundle\XhprofBundle\Entity;
 
 use iXHProfRuns;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class XhguiRuns implements iXHProfRuns, ContainerAwareInterface
 {
-    use ContainerAwareTrait;
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Sets the Container associated with this Controller.
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
     public function __construct($serverName, $uri) {
     	$this->serverName = $serverName;
